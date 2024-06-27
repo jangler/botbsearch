50d50b0b4006bc8cc433dccb7101bcf4a69ec05c
Casey Mulcahy <caseymulcahy@proton.me>
2024-06-27 11:24:28 -0400

    Add status indicator

--- search.js
+++ search.js
@@ -1,3 +1,5 @@
+let statusMap = new Map();
+
 // add a result to an unordered list
 function addResult(ul, typestr, url, title) {
   let li = document.createElement('li');
@@ -9,13 +11,31 @@ function addResult(ul, typestr, url, title) {
   ul.appendChild(li);
 }
 
+// update status for search type
+function setStatus(typestr, status) {
+  statusMap.set(typestr, status);
+  let p = document.querySelector('#status');
+  let vs = [...statusMap.values()];
+  if (vs.includes('waiting')) {
+    p.textContent = 'Searching...';
+  } else if (vs.includes('error')) {
+    p.textContent = 'API error!';
+  } else {
+    p.textContent = 'Done!';
+  }
+}
+
 // call loadfunc on an botb API xmlhttprequest for the given endpoint
-function searchEndpoint(endpoint, query, loadfunc) {
-  let req = new XMLHttpRequest();
-  req.addEventListener('load', (event) => loadfunc(req));
-  req.open('GET', 'https://battleofthebits.com/api/v1/' + endpoint + encodeURIComponent(query));
-  req.responseType = 'json';
-  req.send();
+function searchEndpoint(endpoint, query, typestr, loadfunc) {
+  setStatus(typestr, 'waiting');
+  fetch('https://battleofthebits.com/api/v1/' + endpoint + encodeURIComponent(query))
+    .then(response => {
+      setStatus(typestr, 'done');
+      response.json().then(loadfunc);
+    })
+    .catch(error => {
+      setStatus(typestr, 'error');
+    });
 }
 
 // run when dom content loads
@@ -25,16 +45,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const qinput = document.getElementById('qinput');
     qinput.value = query;
     const results = document.getElementById('results');
-    searchEndpoint('battle/search/', query, (req) =>
-      req.response.forEach(e => addResult(results, 'Battle', e.url, e.title)));
-    searchEndpoint('botbr/search/', query, (req) =>
-      req.response.forEach(e => addResult(results, 'BotBr', e.profile_url, e.name)));
-    searchEndpoint('entry/search/', query, (req) =>
-      req.response.forEach(e => addResult(results, 'Entry', e.profile_url, e.title)));
-    searchEndpoint('group_thread/search/', query, (req) =>
-      req.response.forEach(e => addResult(results, 'Thread',
+    searchEndpoint('battle/search/', query, 'Battle', (data) =>
+      data.forEach(e => addResult(results, 'Battle', e.url, e.title)));
+    searchEndpoint('botbr/search/', query, 'BotBr', (data) =>
+      data.forEach(e => addResult(results, 'BotBr', e.profile_url, e.name)));
+    searchEndpoint('entry/search/', query, 'Entry', (data) =>
+      data.forEach(e => addResult(results, 'Entry', e.profile_url, e.title)));
+    searchEndpoint('group_thread/search/', query, 'Thread', (data) =>
+      data.forEach(e => addResult(results, 'Thread',
         'https://battleofthebits.com/academy/GroupThread/' + e.id + '/', e.title)));
-    searchEndpoint('lyceum_article/search/', query, (req) =>
-      req.response.forEach(e => addResult(results, 'Lyceum', e.profile_url, e.title)));
+    searchEndpoint('lyceum_article/search/', query, 'Lyceum', (data) =>
+      data.forEach(e => addResult(results, 'Lyceum', e.profile_url, e.title)));
   }
 });
